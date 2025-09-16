6f6e9a2b2702c5afc682ce34fc155456bdfacdd4
jappejappe <gabrieljappemoura95071@gmail.com>
2025-09-16 17:06:33 -0300

    Feature: DB

---
+++ backend/database/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE public.emission_records (
+	id_record serial NOT NULL,
+	person_name varchar NOT NULL,
+	emission_amount numeric NOT NULL,
+	distance numeric (5, 2) NOT NULL,
+	people_amount int NOT NULL,
+	vehicle varchar NOT NULL,
+	fuel varchar NOT NULL,
+	CONSTRAINT emission_records_pk PRIMARY KEY (id_record)
+);
