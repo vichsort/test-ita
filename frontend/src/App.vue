5b3f09c4a85a3ab0a95029b0f9099578542e3707
Gustavo Peretti <gustavoperetti11@gmail.com>
2025-09-29 09:45:27 -0300

    feat: logos na navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,33 +1,75 @@
 <template>
   <div>
     <nav>
-      <router-link to="/">Início</router-link> |
-      <router-link to="/forms">Formulário de Consórcio</router-link>
+      <img src="./assets/ifc.png">
+      <div class="links">
+        <router-link to="/">Início</router-link>
+        <router-link to="/forms">Formulário de Consórcio</router-link>
+      </div>
+      <img src="./assets/ita.png">
     </nav>
     
     <router-view></router-view>
   </div>
 </template>
 
-<script setup>
-
-</script>
-
 <style>
-nav {
-  padding: 30px;
-  text-align: center;
-}
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  margin: 0 10px;
-}
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>  box-sizing: border-box;
-  background-color: #f9f9f9;
-  border-radius: 8px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-}
+  body {
+    overflow-x: hidden;
+  }
+
+  nav {
+    padding: 20px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+    text-align: center;
+  }
+
+  nav img {
+    height: 80px;
+  }
+
+  nav img:first-child {
+    justify-self: start;
+  }
+
+  nav .links {
+    display: flex;
+    justify-content: center;
+    justify-self: center;
+  }
+
+  nav img:last-child {
+    justify-self: end;
+  }
+
+  nav a {
+    font-weight: bold;
+    color: #21314a;
+    margin: 0 10px;
+    text-decoration: none;
+    font-size: 1.25em;
+  }
+
+  nav a.router-link-exact-active {
+    color: #0d6efd;
+  }
+
+  @media (max-width: 1024px) {
+    nav .links {
+      flex-direction: column;
+    }
+  }
+
+  @media (max-width: 820px) {
+    nav img {
+      height: 65px;
+    }
+
+    nav a {
+      font-size: 1em;
+    }
+  }
+
+</style>
